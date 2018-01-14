ec53b9b3084f0beb50e4a84d36bbd044ff6aa718
Weilin Shi <934587911@qq.com>
2018-01-14 21:48:01 +0800

    Add 2 more tags

--- src/view/checkin.vue
+++ src/view/checkin.vue
@@ -59,6 +59,16 @@
 					</div>
 				</div>
 			</div>
+			<div class="mint-cell">
+				<div class="mint-cell-wrapper">
+					<div class="mint-cell-title">
+						<checkbox :checked.sync="tagTable['日志']" value="日志"></checkbox>
+					</div>
+					<div class="mint-cell-title">
+						<checkbox :checked.sync="tagTable['文宣']" value="文宣"></checkbox>
+					</div>
+				</div>
+			</div>
 			<div class="d-flex">
 				<div class="flex-0 p-8">
 					<mt-button type="primary"
@@ -81,164 +91,170 @@
 </template>
 
 <script>
-import checkbox from '../component/checkbox.vue';
-import { toast } from 'mint-ui';
+import checkbox from "../component/checkbox.vue";
+import { toast } from "mint-ui";
 
 export default {
-	name: 'checkin',
-	components: { checkbox },
-	data() {
-		return {
-			inputNamePopVis: false,
-			sheetVis: false,
-			editName: '',
-			actionIndex: -1,
-			tagTable: {
-				'负责人': false,
-				'熬粥': false,
-				'前行': false,
-				'摄影': false,
-				'环保': false,
-				'签到': false,
-				'新人': false,
-				'结行': false,
-				'后勤': false
-			}
-		};
-	},
-	computed: {
-		actions() {
-			const name = this.names[this.actionIndex];
-			return [
-				{
-					name: '删除 ' + name,
-					method: this.deleteName
-				},
-				{
-					name: '编辑 ' + name,
-					method: this.modifyName
-				}
-			]
-		},
-		names() {
-			return this.$store.state.names;
-		},
-		tags() {
-			return this.$store.getters.tags;
-		},
-		nameList() {
-			return this.names.map((name, i) => {
-				const tagList = this.tags[i];
-				const tagSurfix = tagList.join('') ? '（' + tagList.join('，') + '）' : '';
-				return {
-					name: (i + 1) + '、' + name + tagSurfix,
-					index: i
-				};
-			});
-		},
-		hasName() {
-			return this.editName.length > 0;
-		},
-		hasValidName() {
-			if (!this.hasName) {
-				return false;
-			}
+  name: "checkin",
+  components: { checkbox },
+  data() {
+    return {
+      inputNamePopVis: false,
+      sheetVis: false,
+      editName: "",
+      actionIndex: -1,
+      tagTable: {
+        负责人: false,
+        熬粥: false,
+        前行: false,
+        摄影: false,
+        环保: false,
+        签到: false,
+        新人: false,
+        结行: false,
+        后勤: false,
+        日志: false,
+        文宣: false
+      }
+    };
+  },
+  computed: {
+    actions() {
+      const name = this.names[this.actionIndex];
+      return [
+        {
+          name: "删除 " + name,
+          method: this.deleteName
+        },
+        {
+          name: "编辑 " + name,
+          method: this.modifyName
+        }
+      ];
+    },
+    names() {
+      return this.$store.state.names;
+    },
+    tags() {
+      return this.$store.getters.tags;
+    },
+    nameList() {
+      return this.names.map((name, i) => {
+        const tagList = this.tags[i];
+        const tagSurfix = tagList.join("") ? "（" + tagList.join("，") + "）" : "";
+        return {
+          name: i + 1 + "、" + name + tagSurfix,
+          index: i
+        };
+      });
+    },
+    hasName() {
+      return this.editName.length > 0;
+    },
+    hasValidName() {
+      if (!this.hasName) {
+        return false;
+      }
 
-			if (this.editName.indexOf('、') !== -1) {
-				return false;
-			}
+      if (this.editName.indexOf("、") !== -1) {
+        return false;
+      }
 
-			const dupNameIndex = this.names.indexOf(this.editName);
-			if (dupNameIndex !== -1 && dupNameIndex !== this.actionIndex) {
-				return false;
-			}
+      const dupNameIndex = this.names.indexOf(this.editName);
+      if (dupNameIndex !== -1 && dupNameIndex !== this.actionIndex) {
+        return false;
+      }
 
-			if (this.editName.length > 7) {
-				return false;
-			}
+      if (this.editName.length > 7) {
+        return false;
+      }
 
-			return true;
-		},
-		hideTabbar() {
-			return this.$store.state.hideTabbar;
-		}
-	},
-	methods: {
-		addName() {
-			this.actionIndex = -1;
-			this.inputNamePopVis = true;
-			// clear input
-			this.editName = '';
-			Object.keys(this.tagTable).forEach(key => {
-				this.tagTable[key] = false;
-			});
-		},
-		confirmName() {
-			const tagList = Object.keys(this.tagTable).filter(key => {
-				return this.tagTable[key];
-			});
-			const action = this.actionIndex > -1 ? 'editName' : 'addName';
+      return true;
+    },
+    hideTabbar() {
+      return this.$store.state.hideTabbar;
+    }
+  },
+  methods: {
+    addName() {
+      this.actionIndex = -1;
+      this.inputNamePopVis = true;
+      // clear input
+      this.editName = "";
+      Object.keys(this.tagTable).forEach(key => {
+        this.tagTable[key] = false;
+      });
+    },
+    confirmName() {
+      const tagList = Object.keys(this.tagTable).filter(key => {
+        return this.tagTable[key];
+      });
+      const action = this.actionIndex > -1 ? "editName" : "addName";
 
-			this.$store.dispatch(
-				action,
-				{ name: this.editName, tagList, index: this.actionIndex }
-			).catch(this.onSyncError);
-			this.inputNamePopVis = false;
-		},
-		deleteName() {
-			this.$store.dispatch('deleteName', this.actionIndex)
-				.catch(this.onSyncError);
-		},
-		onSyncError(err) {
-			this.$toast('同步失败：' + err);
-		},
-		modifyName() {
-			this.inputNamePopVis = true;
-			// refill input
-			this.editName = this.names[this.actionIndex];
-			Object.keys(this.tagTable).forEach(key => {
-				this.tagTable[key] = this.tags[this.actionIndex].indexOf(key) !== -1;
-			});
-		},
-		showAction(index) {
-			this.actionIndex = index;
-			this.sheetVis = true;
-		}
-	},
-	watch: {
-		sheetVis(newValue) {
-			if (newValue) {
-				this.$store.commit('hideTabbar');
-			} else {
-				this.$store.commit('showTabbar');
-			}
-		},
-		inputNamePopVis(newValue) {
-			if (newValue) {
-				this.$store.commit('hideTabbar');
-			} else {
-				this.$store.commit('showTabbar');
-			}
-		}
-	}
-}
+      this.$store
+        .dispatch(action, {
+          name: this.editName,
+          tagList,
+          index: this.actionIndex
+        })
+        .catch(this.onSyncError);
+      this.inputNamePopVis = false;
+    },
+    deleteName() {
+      this.$store
+        .dispatch("deleteName", this.actionIndex)
+        .catch(this.onSyncError);
+    },
+    onSyncError(err) {
+      this.$toast("同步失败：" + err);
+    },
+    modifyName() {
+      this.inputNamePopVis = true;
+      // refill input
+      this.editName = this.names[this.actionIndex];
+      Object.keys(this.tagTable).forEach(key => {
+        this.tagTable[key] = this.tags[this.actionIndex].indexOf(key) !== -1;
+      });
+    },
+    showAction(index) {
+      this.actionIndex = index;
+      this.sheetVis = true;
+    }
+  },
+  watch: {
+    sheetVis(newValue) {
+      if (newValue) {
+        this.$store.commit("hideTabbar");
+      } else {
+        this.$store.commit("showTabbar");
+      }
+    },
+    inputNamePopVis(newValue) {
+      if (newValue) {
+        this.$store.commit("hideTabbar");
+      } else {
+        this.$store.commit("showTabbar");
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
 #fab {
-	position: fixed;
-	right: 20px;
-	bottom: 75px;
-	color: white;
+  position: fixed;
+  right: 20px;
+  bottom: 75px;
+  color: white;
 }
 #fab > div {
-	background-color: #26a2ff;
+  background-color: #26a2ff;
 }
 #name-list {
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	overflow-y: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  overflow-y: auto;
 }
 </style>
